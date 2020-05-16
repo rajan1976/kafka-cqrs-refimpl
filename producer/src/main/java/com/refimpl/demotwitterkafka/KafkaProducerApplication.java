69576ba8b3be5a3c8e9f86427154a6d750444b30
rajan1976 <65379033+rajan1976@users.noreply.github.com>
2020-05-16 10:29:14 -0400

    Add files via upload

---
+++ producer/src/main/java/com/refimpl/demotwitterkafka/KafkaProducerApplication.java
@@ -0,0 +1,75 @@
+package com.refimpl.demotwitterkafka;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+import twitter4j.FilterQuery;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+
+@SpringBootApplication
+@Slf4j
+public class KafkaProducerApplication {
+
+	public static void main(String[] args) {
+		// Kafka config
+		Properties properties = new Properties();
+		properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); //Kafka cluster hosts.
+		properties.put(ProducerConfig.CLIENT_ID_CONFIG, "demo-twitter-kafka-application-producer"); // Group id
+		properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); 
+		properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		Producer<String, String> producer = new KafkaProducer<>(properties);
+
+		// Twitter Stream
+		final TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+		final StatusListener listener = new StatusListener() {
+			public void onStatus(Status status) {
+				final long likes = getLikes(status);
+				final String tweet = status.getText();
+				final String content = status.getUser().getName() + "::::" + tweet + "::::" + likes;
+				log.info(content);
+				producer.send(new ProducerRecord<>("tweets", content));
+			}
+
+			public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+			}
+
+			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+			}
+
+			public void onException(Exception ex) {
+				log.error("Unable to get tweets", ex);
+			}
+
+			@Override
+			public void onScrubGeo(long userId, long upToStatusId) {
+			}
+
+			@Override
+			public void onStallWarning(StallWarning warning) {
+			}
+
+			private long getLikes(Status status) {
+				return status.getRetweetedStatus() != null ? status.getRetweetedStatus().getFavoriteCount() : 0;
+			}
+		};
+		twitterStream.addListener(listener);
+		final FilterQuery tweetFilterQuery = new FilterQuery();
+		tweetFilterQuery.track(new String[] { "FannieMae" });
+		twitterStream.filter(tweetFilterQuery);
+		SpringApplication.run(KafkaProducerApplication.class, args);
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> producer.close())); //Kafka producer should close when application finishes.
+	}
+}
---
+++ reader/target/classes/com/refimpl/demotwitterkafkamysql/repository/InfluencerRepository.kt
@@ -0,0 +1,18 @@
+package com.refimpl.demotwitterkafkamysql.repository
+
+import com.refimpl.demotwitterkafkamysql.model.Influencer
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
+
+@Repository
+interface InfluencerRepository : JpaRepository<Influencer, String> {
+
+    fun findTop10ByOrderByLikesDesc() : List<Influencer>
+
+    fun findTop10ByOrderByTweetsDesc() : List<Influencer>
+
+    @Query("select sum(tweets) from Influencer")
+    fun findTotalTweets() : Int
+
+}
